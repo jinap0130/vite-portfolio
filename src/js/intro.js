e5314613a80db2fc30cf39f9b71a7d0c4f36fd68
Jina Park <jinap0130@gmail.com>
2024-04-17 14:26:06 +0900

    01.intro 페이지 완료

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,1 +1,23 @@
-export function intro(){}
+// import moduleName from 'gsap'
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+export function intro(){
+    gsap.registerPlugin(ScrollTrigger)
+    const frameCount=14
+    let offsetValue=200
+    // (너비)씩 이동해라
+    gsap.to('.img',{
+        backgroundPosition:(-offsetValue*frameCount)+'px',
+        ease:'steps('+frameCount+')',
+        scrollTrigger:{
+            trigger:'#intro',
+            start:'top top',
+            end:'+='+(frameCount*offsetValue),
+            pin:true,
+            scrub:true,
+
+
+        } 
+    })
+}
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,1 +1,15 @@
-export function menu(){}
+export function menu(){
+    const headerToggle=document.getElementById('headerToggle')
+    const headerNav=document.querySelector('.header_nav')
+
+    if(headerToggle){
+        headerToggle.addEventListener('click',function(){
+            headerNav.classList.toggle('show')
+            headerToggle.getAttribute('aria-expanded') == 'true'
+            ? headerToggle.setAttribute('aria-expanded','false')
+            : headerToggle.setAttribute('aria-expanded','true');
+
+            // (조건식)?(참이면);(거짓이면)^^ if - else 구문을 표햠 
+        })
+    }
+}
