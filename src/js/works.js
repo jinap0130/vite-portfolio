f110a64c8023084da615b8ca2ec95f185225d8bf
Jina Park <jinap0130@gmail.com>
2024-04-19 14:21:49 +0900

    04. work/smooth scroll

--- src/js/works.js
+++ src/js/works.js
@@ -1,1 +1,24 @@
-export function works(){}
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+export function works(){
+    gsap.registerPlugin(ScrollTrigger)
+    const horSection=gsap.utils.toArray('.work_item')
+    gsap.to(horSection, {
+        xPercent:-100*(horSection.length -1),
+        ease:"none",
+        scrollTrigger:{
+            trigger:"#work",
+            start: 'top 56px',//header뺀 나머지
+            end:'+=2380px',//전체 width
+            pin: true,
+            scrub:1, 
+
+            invalidateOnRefresh:true,
+            anticipatePin:1,
+
+        }
+    })
+}
+
+// ^^가로 스크롤 제어
